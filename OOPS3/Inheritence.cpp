c3979856427b3f89c24a9e4a4fda11c8c285bc6c
Ishan Pandey <Ishan.parasar.3359@gmail.com>
2019-08-08 03:48:37 +0530

    Create Inheritence.cpp

---
+++ OOPS3/Inheritence.cpp
@@ -0,0 +1,8 @@
+// Inheritence : to avoid writing common properties in different classes.
+// add other properties to classes that are specific to those.
+
+// Access Modifiers: private, public, protected.
+// in a class:
+// public: accessible by anyone
+// private: used only by this class
+// protected: used by this class and,are accessible outside this class but only by using its child classes.
