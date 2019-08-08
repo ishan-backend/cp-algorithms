3c7cd42d5171969a57bf08f06989176b0a1cb048
Ishan Pandey <Ishan.parasar.3359@gmail.com>
2019-08-08 22:25:37 +0530

    Update OperatorOverloading1_2_3.cpp

--- OOPS2/OperatorOverloading1_2_3.cpp
+++ OOPS2/OperatorOverloading1_2_3.cpp
@@ -85,3 +85,55 @@ int main()
 
 // Operator Overloading 2
 
+
+// Pre-increment
+//we cant make this function const, as were changing values of this->
+        Fraction& operator++() {// & means returned by reference, directly from location where ++f was calculated and thus prevents wastage of buffer
+            numerator = numerator + denominator;
+            simplify();
+
+            return *this;   //returning complete block of num and denom
+        }
+
+int main() {
+    Fraction f1(10, 2);
+    Fraction f2(15, 4);
+
+    f1.print();
+    Fraction f3 = ++(++f1);
+    f1.print();
+    f3.print();
+
+
+
+
+
+    //++f1;
+    //f1.print();
+
+    /*
+    Fraction f3 = ++f1;
+    f1.print();
+    f3.print();
+    */
+    /*
+    Fraction f3 = f1.add(f2);
+    Fraction f4 = f1 + f2;
+    f1.print();
+    f2.print();
+    f3.print();
+    f4.print();
+
+    Fraction f5 = f1 * f2;
+    f5.print();
+
+    if(f1 == f2) {
+        cout << "Equal" << endl;
+    }
+    else {
+        cout << "Not equal " << endl;
+        
+    }
+    */
+}   
+
