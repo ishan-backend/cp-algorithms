242e3225c51ec8280e44a13eaeacc3279b953e2b
Ishan Pandey <Ishan.parasar.3359@gmail.com>
2019-08-08 15:52:52 +0530

    Create Polymorphism(Compile Time).cpp

---
+++ OOPS3/Polymorphism(Compile Time).cpp
@@ -0,0 +1,5 @@
+// polymorphism : many forms
+// our code behaves differently in different situations
+// two types:
+// 1. compile time: acheived using function and operator overloading.
+// 2. run time: 
