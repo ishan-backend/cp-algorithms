a1496bc727854240653dff0dbc164f17906eef0c
Ishan Pandey <Ishan.parasar.3359@gmail.com>
2019-08-08 22:28:44 +0530

    Update OperatorOverloading1_2_3.cpp

--- OOPS2/OperatorOverloading1_2_3.cpp
+++ OOPS2/OperatorOverloading1_2_3.cpp
@@ -137,3 +137,92 @@ int main() {
     */
 }   
 
+//PART 3:
+
+// Post-increment
+		Fraction operator++(int) {
+			Fraction fNew(numerator, denominator);
+			numerator = numerator + denominator;
+			simplify();
+			fNew.simplify();
+			return fNew;
+		}
+
+
+		Fraction& operator+=(Fraction const &f2) {
+			int lcm = denominator * f2.denominator;
+			int x = lcm / denominator;
+			int y = lcm / f2.denominator;
+
+			int num = x * numerator + (y * f2.numerator);
+
+			numerator = num;
+		  denominator = lcm;
+			simplify();
+
+			return *this;
+		}
+
+int main() {
+
+	int i = 5, j = 3;
+	(i += j) += j;
+
+	cout << i << " " << j << endl;
+
+	Fraction f1(10, 3);
+	Fraction f2(5, 2);
+
+	
+	(f1 += f2) += f2;
+
+	f1.print();
+	f2.print();
+
+
+
+	/*
+	Fraction f3 = f1++;
+	f1.print();
+	f3.print();
+	*/
+
+	/*
+	f1.print();
+	Fraction f3 = ++(++f1);
+	f1.print();
+	f3.print();
+	*/
+
+
+
+
+	//++f1;
+	//f1.print();
+
+	/*
+	Fraction f3 = ++f1;
+	f1.print();
+	f3.print();
+	*/
+	/*
+	Fraction f3 = f1.add(f2);
+	Fraction f4 = f1 + f2;
+	f1.print();
+	f2.print();
+	f3.print();
+	f4.print();
+
+	Fraction f5 = f1 * f2;
+	f5.print();
+
+	if(f1 == f2) {
+		cout << "Equal" << endl;
+	}
+	else {
+		cout << "Not equal " << endl;
+		
+	}
+	*/
+}
+
