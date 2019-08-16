62363d27a2bad605f9e495a0bf77b920278d9173
Ishan Pandey <Ishan.parasar.3359@gmail.com>
2019-08-16 20:38:00 +0530

    Create temp1.cpp

---
+++ BasicTemp/temp1.cpp
@@ -0,0 +1,68 @@
+/* Author: Ishan Pandey */
+/* All the Gods, All the Heavens, All the Hells are within you. */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define int long long
+#define all(sdk) sdk.begin(),sdk.end()
+#define mll map<int, int>
+#define pb push_back
+#define eb emplace_back
+#define rep(n) for(int i=0;i<n;++i)
+#define repa(i,a,b) for(int i=a;i<=b;++i)
+#define repd(i,a,b) for(int i=a;i>=b;--i)
+#define mp make_pair
+#define endl '\n'
+#define vvll vector<vector<int> >
+#define vll vector<int>
+#define mint map<int,int> 
+#define sz(x) (int)x.size()
+#define sll set<int>
+#define pll pair<int,int>
+#define F first
+#define S second
+#define gc getchar()
+const long double PI = acos(-1.0);
+const int mod = 1000*1000*1000 + 7;
+
+inline int add(int a, int b){ return a+b >= mod ? a+b-mod : a+b;}
+inline void inc(int &a, int b){ a = add(a, b);}
+inline int sub(int a, int b){ return a-b < 0 ? a-b+mod : a-b;}
+inline void dec(int &a, int b){ a = sub(a, b);}
+
+
+#define prec(x, n) fixed<<setprecision(n)<<x
+
+void start_func()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+
+void solve()
+{
+  
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    start_func();
+    
+    int t=1;
+    //cin>>t;
+    while(t--)
+    {
+      solve();
+    }
+    
+    return 0;
+}
