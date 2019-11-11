0a8df27f5f445b05f4f2bf53d2aebc03e82d0a78
Ishan Pandey <Ishan.parasar.3359@gmail.com>
2019-11-11 20:09:34 +0530

    Update usingBIT.cpp

--- Inversions_count/usingBIT.cpp
+++ Inversions_count/usingBIT.cpp
@@ -67,3 +67,81 @@
        return 0;
     } 
 
+
+
+
+//SPOJ YODANESS:
+#include <iostream>
+
+#include<bits/stdc++.h>
+using namespace std;
+#define llong long long 
+const int MAXN = 300020;
+llong tree[MAXN];
+
+//using BIT:
+
+llong read(int idx)
+{
+    llong sum = 0;
+    while (idx > 0)
+    {
+        sum += tree[idx];
+        idx -= (idx & -idx);
+    }
+    return sum;
+}
+
+void update(int val, int idx, int n)
+{
+    while (idx <= n)
+    {
+        tree[idx] += val;
+        idx += (idx & -idx);
+    }
+}
+
+
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        memset(tree, 0, sizeof(tree));   
+
+        map<string, int> mymap;
+        int temp=n;
+        for(int i=1; i<=n; i++) //keeping array/map 1-based
+        {
+            string str;
+            cin>>str;
+            mymap[str] = i;
+        }
+        int* arr = new int[n+1];
+
+        for(int i=1; i<=n; i++) //keeping array/map 1-based
+        {
+            string str;
+            cin>>str;
+            arr[i] = mymap[str];
+        }
+
+        int invcnt=0;
+        for(int i=n; i>=1; i--)
+        {
+            invcnt += read(arr[i]);
+            update(1, arr[i], n);
+        }
+
+        mymap.clear();
+        cout<<invcnt<<endl;
+
+    }
+    return 0;
+}
+
+
