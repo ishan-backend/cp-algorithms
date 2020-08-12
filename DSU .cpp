c827fb28432f9594aa0f1c0df2e349da19ea7a29
Ishan Pandey <Ishan.parasar.3359@gmail.com>
2020-08-12 16:05:10 +0530

    Create DSU .cpp

---
+++ DSU .cpp
@@ -0,0 +1,25 @@
+const int N  = 2001;
+int parent[N], rank1[N];
+
+void make_set(int v) {
+    parent[v] = v;
+    rank1[v] = 0;
+}
+
+int find_set(int v) {
+    if (v == parent[v])
+        return v;
+    return parent[v] = find_set(parent[v]);
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (rank1[a] < rank1[b])
+            swap(a, b);
+        parent[b] = a;
+        if (rank1[a] == rank1[b])
+            rank1[a]++;
+    }
+}
